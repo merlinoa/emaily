0c63f2b67e8772d35774851e3dc8a434553c1033
merlinoa <merlinoa88@gmail.com>
2017-08-02 14:44:28 -0400

    hooked up redux and react router

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+// redux data layer
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'  
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App'
+import reducers from './reducers'
+
+const store = createStore(reducers, {}, applyMiddleware())
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+)
