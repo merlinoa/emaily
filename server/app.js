1be31f2922e0ebc4e67cac0e5f2f05a3424ee7fe
merlinoa <merlinoa88@gmail.com>
2017-08-04 08:51:48 -0400

    prep for deployment

--- server/app.js
+++ server/app.js
@@ -11,8 +11,6 @@ mongoose.connect(keys.mongoURI)
 
 const app = express()
 
-// sets express to always start looking for files in the root directory
-// app.use(express.static(__dirname))
 // allows for parsing of req.body
 app.use(bodyParser.json())
 
@@ -24,12 +22,24 @@ app.use(cookieSession({
 app.use(passport.initialize())
 app.use(passport.session())
 
+// routes that go directly to Express APIs
 require('./routes/auth')(app)
 require('./routes/billing')(app)
 
-app.get('/', (req, res) => {
-    res.send({name: "Andy"})
-})
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up React production assest
+    // like our main.js React stuff
+    // make root at client/build.  This will happen after express
+    // serves index.html; which is set up in the code following this loc
+    app.use(express.static('client/build'))
+
+    // if Express does not recognize the route
+    // serve up index.html which will then look for the react js build file
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
